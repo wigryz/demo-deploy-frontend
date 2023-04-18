5122f5af6b0ded3ee9a244af6453cb83f1eccc76
Tomasz Ligęza <tomasz.ligeza3@gmail.com>
2023-04-18 16:15:21 +0200

    remove not needed code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import {useRoutes, BrowserRouter as Router} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Hello() {
+  return <div>Hello</div>;
 }
 
-export default App;
+class Backend extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { response: '' };
+  }
+
+  componentDidMount() {
+    axios.get(process.env.REACT_APP_BACKEND_URL)
+        .then(response => {
+          this.setState({ response: response.data });
+        })
+        .catch(error => {
+          console.error(error);
+        });
+  }
+
+  render() {
+    return (
+        <div>
+          Backend response (url: {process.env.REACT_APP_BACKEND_URL}): {this.state.response}
+        </div>
+    );
+  }
+}
+
+function App() {
+    const routes = useRoutes([
+        { path: '/', element: <Hello /> },
+        { path: '/backend', element: <Backend /> },
+        // other routes
+    ]);
+
+    return (
+        <div>
+            {routes}
+        </div>
+    );
+}
+
+function MyRouter() {
+    return (
+        <Router>
+            <App />
+        </Router>
+    );
+}
+
+export default MyRouter;
